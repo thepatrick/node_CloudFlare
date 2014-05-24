43a02ba98b6ed21a48524901ec7135b2a15edc80
Patrick Quinn-Graham <make-contact@pftqg.com>
2014-05-24 23:23:19 +1000

    Update node_CloudFlare.js

--- node_CloudFlare.js
+++ node_CloudFlare.js
@@ -1,64 +1,47 @@
-(function ()
+var ranges = require('./ranges.json');
+
+exports.check = function(req)
 {
-    function isEmpty(obj)
+    var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
+    if (typeof req.headers['cf-connecting-ip'] === 'undefined')
     {
-        return Object.keys(obj).length === 0;
+        return false;
     }
-
-    var ranges = require('./ranges.json');
-
-    function check(req)
+    else
     {
-        var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
-        if (typeof req.headers['cf-connecting-ip'] === 'undefined')
+        var range_check = require('range_check');
+        if (range_check.vaild_ip(ip_address))
         {
-            return false;
-        }
-        else
-        {
-            var range_check = require('range_check');
-            if (range_check.vaild_ip(ip_address))
+            var ip_ver = range_check.ver(ip_address);
+            if (ip_ver === 4)
             {
-                var ip_ver = range_check.ver(ip_address);
-                if (ip_ver === 4)
-                {
-                    return range_check.in_range(ip_address, ranges.v4);
-                }
-                else if (ip_ver === 6)
-                {
-                    return range_check.in_range(ip_address, ranges.v6);
-                }
-                else
-                {
-                    return false;
-                }
+                return range_check.in_range(ip_address, ranges.v4);
+            }
+            else if (ip_ver === 6)
+            {
+                return range_check.in_range(ip_address, ranges.v6);
             }
             else
             {
                 return false;
             }
         }
-    }
-
-    function get(req)
-    {
-        var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
-        if (typeof req.headers['cf-connecting-ip'] === 'undefined')
-        {
-            return ip_address;
-        }
         else
         {
-            return req.headers['cf-connecting-ip'];
+            return false;
         }
     }
+}
 
-    // Export public API
-    var node_CloudFlare = {};
-
-    node_CloudFlare.load = load;
-    node_CloudFlare.check = check;
-    node_CloudFlare.get = get;
-
-    module.exports = node_CloudFlare;
-}());
+exports.get = function(req)
+{
+    var ip_address = (req.connection.remoteAddress ? req.connection.remoteAddress : req.remoteAddress);
+    if (typeof req.headers['cf-connecting-ip'] === 'undefined')
+    {
+        return ip_address;
+    }
+    else
+    {
+        return req.headers['cf-connecting-ip'];
+    }
+}
